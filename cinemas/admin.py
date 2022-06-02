0cfdde127dda711250cf11c908dcc87dfbeefc16
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-02 16:26:24 +0300

    changed db relations on cinemas models

--- cinemas/admin.py
+++ cinemas/admin.py
@@ -4,22 +4,15 @@ from .models import *
 
 
 class HallAdmin(admin.ModelAdmin):
-    list_display = ['name', 'show_seats_pk']
-
-    def show_seats_pk(self, obj):
-        return ", ".join([str(a.pk) for a in obj.seats.all()])
+    list_display = ['name', 'movie_theater']
 
 
-class AdminMovieTheater(admin.ModelAdmin):
-    list_display = ['name', 'show_halls']
 
-    def show_halls(self, obj):
-        return ", ".join([a.name for a in obj.halls.all()])
 
 class AdminSeat(admin.ModelAdmin):
-    list_display = ['pk', 'seat_row','seat_nr']
+    list_display = ['pk', 'seat_row', 'seat_nr', 'halls']
 
 
 admin.site.register(Seat, AdminSeat)
 admin.site.register(Hall, HallAdmin)
-admin.site.register(MovieTheater,AdminMovieTheater)
+admin.site.register(MovieTheater)
--- cinemas/models.py
+++ cinemas/models.py
@@ -3,21 +3,9 @@ from locations.models import County, City, Street
 # Create your models here.
 
 
-class Seat(models.Model):
-    seat_nr = models.CharField(max_length=10, unique=True)
-    seat_row = models.CharField(max_length=10, default='')
-    is_reserved = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f'{self.seat_row} {self.seat_nr}'
-
-
-class Hall(models.Model):
-    name = models.CharField(max_length=200, blank=False)
-    seats = models.ManyToManyField(Seat, related_name='seats')
-
-    def __str__(self):
-        return self.name
+from django.db import models
+from locations.models import County, City, Street
+# Create your models here.
 
 
 class MovieTheater(models.Model):
@@ -25,10 +13,27 @@ class MovieTheater(models.Model):
     county = models.ForeignKey(County, null=True, on_delete=models.SET_NULL)
     city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL)
     street = models.ForeignKey(Street, null=True, on_delete=models.SET_NULL)
-    halls = models.ManyToManyField(Hall)
 
     def __str__(self):
         return self.name
+
+
+class Hall(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    movie_theater = models.ForeignKey(MovieTheater, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Seat(models.Model):
+    seat_nr = models.CharField(max_length=10, unique=True)
+    seat_row = models.CharField(max_length=10, default='')
+    is_reserved = models.BooleanField(default=False)
+    halls = models.ForeignKey(Hall, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.seat_row} {self.seat_nr}'
 
 
 
