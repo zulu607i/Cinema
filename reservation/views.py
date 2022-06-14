fd40a32ac4a9dc60d69a5874fd0fc290a77b5b75
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-14 15:44:51 +0300

    fixed get_csv method

--- reservation/views.py
+++ reservation/views.py
@@ -9,10 +9,12 @@ from cinema.settings import EMAIL_HOST_USER
 from django.contrib.auth.decorators import login_required
 from cinemas.models import Hall
 import csv
+from ratelimit.decorators import ratelimit
 # Create your views here.
 
 
 @login_required()
+@ratelimit(key="ip", rate="30/m", block=True)
 def book_a_ticket(request, pk, hall_pk):
     movie = Movie.objects.get(pk=pk)
     hall = Hall.objects.get(pk=hall_pk)
@@ -37,13 +39,14 @@ def book_a_ticket(request, pk, hall_pk):
 
     return render(request, 'reservation/reserve.html', {'form': form,})
 
-def get_csv_file(request, pk):
-    user = User.objects.get(pk=pk)
+@login_required()
+@ratelimit(key="ip", rate="30/m", block=True)
+def get_csv_file(request):
     reservations = Reservation.objects.filter(user=request.user)
-
     if request.method == "POST":
         response = HttpResponse(content_type='text/csv')
-        response['Content=-Disposition'] = f'attachment; filename=reservation_{user.username}.csv'
+        filename = f'{request.user}-reservations'
+        response['Content=-Disposition'] = f'attachment; filename={filename}.csv'
         writer = csv.writer(response)
         writer.writerow(['ID', 'User', 'Hall', 'Movie', 'Seat', 'Start time'])
         for r in reservations:
