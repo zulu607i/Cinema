0ce71bbc2d8baca93dbc6975a21c9dae564cd7e4
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-15 11:25:08 +0300

    added movie details page

--- ui/urls.py
+++ ui/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('contact/', views.contact_form, name='contact_form'),
+    path('details/<int:pk>/', views.MovieDetailView.as_view(), name='movie_details'),
 ]
