e250d8b761325d70259e979c681b1d15905fd872
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-16 17:25:56 +0300

    added email verification

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    path('pre_register', pre_register, name='pre_register'),
+    path("register/<slug:uidb64>/", register_view, name="register"),
     path('activation_sent', activation_sent, name='activation_sent'),
     path('activate/<slug:uidb64>/<slug:token>/', activate_user_profile, name='activate_profile'),
     path('login/', loginPage, name='login'),
