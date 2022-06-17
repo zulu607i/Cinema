4270b58362a073dc21b795670c03bbf2588b65fe
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-17 10:48:43 +0300

    deleted prints

--- reservation/tasks.py
+++ reservation/tasks.py
@@ -4,11 +4,11 @@ from .models import *
 def expired_reservations():
     reservation = Reservation.objects.all()
     for r in reservation:
-        exipired_time = r.playing_time.start_time - timedelta(minutes=30)
-        print(exipired_time)
+        expired_time = r.playing_time.start_time - timedelta(minutes=30)
+
         if not r.is_confirmed:
-            if r.playing_time.start_time > exipired_time:
+            if r.playing_time.start_time > expired_time:
                 r.expired = True
                 r.save()
-                print(r.expired)
+
             return r
