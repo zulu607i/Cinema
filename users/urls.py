3fddb280030fd2c0ce1a50819b99f457cd6a21f1
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-22 09:22:12 +0300

    disabled user confrimation

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('pre_register', pre_register, name='pre_register'),
     path("register/<slug:uidb64>/", register_view, name="register"),
     path('activation_sent', activation_sent, name='activation_sent'),
-    path('activate/<slug:uidb64>/<slug:token>/', activate_user_profile, name='activate_profile'),
+
     path('login/', loginPage, name='login'),
     path('logout/', logoutPage, name='logout'),
 ]
