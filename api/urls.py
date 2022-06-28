8c940f48c4efc4807411e53a579c1ef42c37dd03
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-28 11:20:15 +0300

    added movies currently playing this week with datetime, hall

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from rest_framework import routers
 
-from api.views import MoviesPlayingThisWeekViewSet
+from api.views import MoviesPlayingThisWeekViewSet, MoviesPlayingThisWeekDetailsViewSet
 
 router = routers.DefaultRouter()
 
 router.register(
     "movies-playing-this-week", MoviesPlayingThisWeekViewSet, "assigned_movies_this_week"
+
+)
+
+router.register(
+    "movies-playing-time-this-week", MoviesPlayingThisWeekDetailsViewSet, 'movies_playing_time_this_week'
 )
