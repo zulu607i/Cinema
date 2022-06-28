20ee1f3b10fe19e91056c3e5ded9393d75e06e50
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-06-28 10:57:54 +0300

    changed url underscore to dash

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from api.views import MoviesPlayingThisWeekViewSet
 router = routers.DefaultRouter()
 
 router.register(
-    "movies_playing_this_week", MoviesPlayingThisWeekViewSet, "assigned_movies_this_week"
+    "movies-playing-this-week", MoviesPlayingThisWeekViewSet, "assigned_movies_this_week"
 )
