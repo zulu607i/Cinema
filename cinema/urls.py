b15e4e8cddc333cea709b084023f08ba72c43bdd
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-07-11 12:26:48 +0300

    deleted unused attributes from get schema view

--- cinema/urls.py
+++ cinema/urls.py
@@ -27,12 +27,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-      title="Snippets API",
+      title="Cinema API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="Cinema API",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
