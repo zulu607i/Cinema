282f3c1908fa9a53bfb312f3d5ff027890e12a71
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-07-22 11:23:17 +0300

    fixed iteration

--- script.py
+++ script.py
@@ -8,29 +8,24 @@ from bs4 import BeautifulSoup
 def connect():
     conn = psycopg2.connect(host='db', user="postgres", database="postgres-2", password='mmuntean1!')
     cur = conn.cursor()
-    cur.execute("SELECT name, description, imdb_id FROM public.movies_movie WHERE movies_movie.description='';")
+    cur.execute("SELECT imdb_id FROM public.movies_movie WHERE movies_movie.description='';")
     print("The number of parts: ", cur.rowcount)
-    row = cur.fetchone()
-    imdb_ids = []
+    rows = cur.fetchall()
+    if rows is not None:
+        for row in rows:
+            url = f'https://www.imdb.com/title/{row[0]}/'
+            response = requests.get(url)
+            html_soup = BeautifulSoup(response.text, 'html.parser')
+            plot = html_soup.find('span', class_='sc-16ede01-2 gXUyNh')
+            if plot is not None:
+                plot_updated = plot.text.replace("'", "`")
+                cur.execute(f"UPDATE public.movies_movie SET description='{plot_updated}' WHERE movies_movie.imdb_id='{row[0]}';", rows)
+                updated_rows = cur.rowcount
+                print(updated_rows)
+                conn.commit()
 
-    while row is not None:
-        row = cur.fetchone()
-        print(row)
-        if row is not None:
-            imdb_ids.append(row[2])
-    for i in imdb_ids:
-        url = f'https://www.imdb.com/title/{i}/'
-        response = requests.get(url)
-        html_soup = BeautifulSoup(response.text, 'html.parser')
-        plot = html_soup.find('span', class_='sc-16ede01-2 gXUyNh')
-        if plot is not None:
-            plot_updated = plot.text.replace("'", "`")
-            cur = conn.cursor()
-            cur.execute(f"UPDATE public.movies_movie SET description='{plot_updated}' WHERE movies_movie.imdb_id='{i}';")
-            updated_rows = cur.rowcount
-            print(updated_rows)
-            conn.commit()
     cur.close()
+
 
 if __name__ == '__main__':
     connect()
