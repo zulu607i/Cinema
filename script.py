efc3d71b806c6ac82e4d34178fba8ba43de25e00
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-07-22 13:45:14 +0300

    update db description from a list of tuples

--- script.py
+++ script.py
@@ -8,22 +8,21 @@ from bs4 import BeautifulSoup
 def connect():
     conn = psycopg2.connect(host='db', user="postgres", database="postgres-2", password='mmuntean1!')
     cur = conn.cursor()
-    cur.execute("SELECT imdb_id FROM public.movies_movie WHERE movies_movie.description='';")
+    cur.execute("SELECT description, imdb_id FROM public.movies_movie WHERE movies_movie.description='';")
+    update_query = "UPDATE public.movies_movie SET description=%s WHERE movies_movie.imdb_id=%s;"
     print("The number of parts: ", cur.rowcount)
-    rows = cur.fetchall()
-    if rows is not None:
-        for row in rows:
-            url = f'https://www.imdb.com/title/{row[0]}/'
-            response = requests.get(url)
-            html_soup = BeautifulSoup(response.text, 'html.parser')
-            plot = html_soup.find('span', class_='sc-16ede01-2 gXUyNh')
-            if plot is not None:
-                plot_updated = plot.text.replace("'", "`")
-                cur.execute(f"UPDATE public.movies_movie SET description='{plot_updated}' WHERE movies_movie.imdb_id='{row[0]}';", rows)
-                updated_rows = cur.rowcount
-                print(updated_rows)
-                conn.commit()
+    update_data = []
+    for row in cur.fetchall():
+        url = f'https://www.imdb.com/title/{row[1]}/'
+        response = requests.get(url)
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        plot = html_soup.find('span', class_='sc-16ede01-2 gXUyNh')
+        if plot is not None:
+            data = (plot.text, row[1])
+            update_data.append(data)
 
+    cur.executemany(update_query, update_data)
+    conn.commit()
     cur.close()
 
 
