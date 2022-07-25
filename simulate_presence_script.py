f6a4505b0cbd19bc4b6d8d047d1a97a598e0221f
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-07-25 11:25:02 +0300

    added random number of seats

--- simulate_presence_script.py
+++ simulate_presence_script.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import random
 seat_seat = 'http://127.0.0.1:8000/api/seats/'
 halls_url = 'http://127.0.0.1:8000/api/halls/'
 payload = {}
@@ -11,16 +12,15 @@ response_seats = requests.request("GET", seat_seat, headers=headers, data=payloa
 response_halls = requests.request('GET', halls_url, headers=headers, data=payload)
 halls = json.loads(response_halls.content)['results']
 seats = json.loads(response_seats.content)['results']
+random_nr_of_seats = random.randint(1, len(seats))
 
-
-for i in seats:
+for i in seats[0:random_nr_of_seats]:
     if i['halls'] == halls[0]['id']:
         seat_id_url = f"http://127.0.0.1:8000/api/seats/{i['id']}/"
         if not i['is_occupied']:
             payload = {'is_occupied': True}
             requests.request("PUT", seat_id_url, headers=headers, data=payload)
-            print(requests.request("PUT", seat_id_url, headers=headers, data=payload))
+
         else:
             payload = {'is_occupied': False}
             requests.request("PUT", seat_id_url, headers=headers, data=payload)
-            print(requests.request("PUT", seat_id_url, headers=headers, data=payload))
