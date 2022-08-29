a8ea6e34efcd223a8abbafa0c5f3aaa325ed3f47
Mircea-Andrei MUNTEAN <andreimmircea1@gmail.com>
2022-08-29 14:36:28 +0300

    ..

--- cinema/settings.py
+++ cinema/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['staging-cinema.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://staging-cinema.herokuapp.com']
